63542cc53c2569328382f436a38bbbb60dfd87cb
Sword-holder <925937463@qq.com>
2019-08-14 22:56:11 +0800

    图论也太难了吧

--- cpp/3.cpp
+++ cpp/3.cpp
@@ -1,91 +1,123 @@
 #include <iostream>
-#include <climits>
 #include <vector>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <map>
+#include <utility>
+#include <list>
 #include <set>
-#define INF 10000000
+#include <climits>
+
 using namespace std;
+
+const int INF = 100000000;
 
 int main() {
 	int N, M, C1, C2;
 	cin >> N >> M >> C1 >> C2;
-	int team_num[N];
-	// 初始化每个城市的救援队数量 
+	
+	// 输入救援队数量 
+	vector<int> rescue(N);
 	for (int i = 0; i < N; i++) {
-		cin >> team_num[i];
+		cin >> rescue[i];
 	}
-	// 初始化邻接表结构
-	// 里面的pair中，第一个元素表示邻接的节点号，第二个元素表示距离 
-	// 这个构造函数中，adj_table()括号里面表示数组初始化长度 
-	int adj_matrix[N][N] = {INF};
-	fill(adj_matrix, adj_matrix + N * N, INF);
+	
+	// 距离表 
+	vector<vector<int> > adj_matrix;
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << adj_matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < N; i++) {
+		adj_matrix.push_back(vector<int>(N, INF));
 		adj_matrix[i][i] = 0;
 	}
+	// 邻接表
+	vector<list<int> > adj_table(N);
 	
+	// 输入道路之间的距离
 	for (int i = 0; i < M; i++) {
-		int c1, c2, dis;
-		cin >> c1 >> c2 >> dis;
-		adj_matrix[c1][c2] = dis;
-		adj_matrix[c2][c1] = dis;
-	}
-	 
-	// pair中存储的是题目要求输出的最短路径个数和最多救援队数量 
-	// results是存储C1到每个点的一对pair值 
-	vector<pair<int, int> > results(N);
-	// C1自己到自己的最短路径数量为1，救援队数量就是C1中救援队的数量 
-	results[C1] = pair<int, int>(1, team_num[C1]);
-	// C1到其它点的最短路径 
-	vector<int> distance(N, INF);
-	distance[C1] = 0; 
+		int source, target, dis = 0;
+		cin >> source >> target >> dis;
+		adj_matrix[source][target] = dis;
+		adj_matrix[target][source] = dis;
+		
+		adj_table[source].push_front(target);
+		adj_table[target].push_front(source);
+	} 
 	
-	// 类似于迪杰斯特拉算法，本质上是动态规划
-	// Q指的是未找到最短路径的点的集合，一开始Q就是所有点 
-	set<int> Q;
+	// 结果表，键表示城市编号，值是一个pair，第一个值为最短路径的数量，第二个值为最大的援救数量 
+	vector<pair<int, int> > result(N);
+	
+	// 迪杰斯特拉算法 
+	// 最短距离数组 
+	vector<int> distance(N);
 	for (int i = 0; i < N; i++) {
-		Q.insert(i);
+		distance[i] = adj_matrix[C1][i];
 	}
 	
-	// new_node表示上一次找到最短路径的节点 
-	while (!Q.empty()) {
-		// 把Q集合遍历一次，找到最小值 
-		int min_dis = INT_MAX, min_node = -1; 
-		for (set<int>::iterator itr = Q.begin(); itr != Q.end(); itr++) {
-			if (distance[*itr] < min_dis) {
-				min_dis = distance[*itr];
-				min_node = *itr;
-			} 
-		}
-		for (int i = 0; i < distance.size(); i++) {
-			cout << distance[i] << " ";
-		}
-		cout << endl << min_node << endl;
-		// 松弛每个节点 
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < N; j++) {
+//			cout << adj_matrix[i][j] << " ";
+//		}
+//		cout << endl;
+//	} 
+//	cout << endl;
+	
+	// 用于记录最短路径的数量 
+	vector<int> path_num(N, 0);
+	// 最大的救援队数量 
+	vector<int> max_rescue(N, rescue[C1]);
+	
+	// 找到/未找到最短路径的结点id集合，实际上是一个数组，1表示已找到，0表示未找到 
+	vector<int> found(N, 0);
+	found[C1] = 1;
+	
+	while (true) {
+		int min_dis = INT_MAX, min_node = -1;
 		for (int i = 0; i < N; i++) {
-			if (distance[min_node] + adj_matrix[min_node][i] < distance[i]) {
-				cout << "distance[i] = " << distance[i];
-				cout << "adj_matrix[min_node][i] = " << adj_matrix[min_node][i];
-				cout << endl;
-				distance[i] = distance[min_node] + adj_matrix[min_node][i];
+			if (found[i] == 0 && distance[i] < min_dis) {
+				min_dis = distance[i];
+				min_node = i;
 			}
 		}
 		
-		// 把最短的那个节点移除 
-		Q.erase(min_node);
+		// 寻找最大救援队
+		// 遍历最小节点相邻的所有结点 
+		int min_num = 0;
+		int max_r = 0;
+		for (list<int>::iterator itr = adj_table[min_node].begin(); itr != adj_table[min_node].end(); itr++) {
+			if (found[*itr] == 1 && min_dis == distance[*itr] + adj_matrix[*itr][min_node]) {
+				min_num++;
+				if (max_rescue[*itr] > max_r) {
+					max_r = max_rescue[*itr];
+				}
+			}
+		}
+		max_rescue[min_node] = max_r + rescue[min_node];
+		// cout << "max rescue " << min_node << ":" << max_rescue[min_node];
+		
+		// 找到了新的最短路径点 
+//		cout << "distance table: ";
+//		for (int i = 0; i < N; i++) {
+//			cout << distance[i] << " ";
+//		}
+//		cout << endl;
+//		cout << "found new node :" << min_node << endl;
+//		
+//		cout << "max rescue: ";
+//		for (int i = 0; i < N; i++) {
+//			cout << max_rescue[i] << " ";
+//		}
+//		cout << endl;
+		
+		path_num[min_node] = min_num;
+		
+		if (min_node == C2) {
+			break;
+		}
+		found[min_node] = 1;
+		if (min_node != -1) {
+			for (int i = 0; i < N; i++) {
+				if (found[i] == 0 && distance[min_node] + adj_matrix[min_node][i] < distance[i]) {
+					distance[i] = distance[min_node] + adj_matrix[min_node][i];
+				}
+			}
+		}
 	}
 	
-	for (int i = 0; i < N; i++) {
-		cout << i << ":" << distance[i] << endl;
-	}
-	//cout << results[C2] << std::endl;
-	return 0;
-} 
+	cout << path_num[C2] << " " << max_rescue[C2] << endl;
+}
